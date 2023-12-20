8c096781e44b617bbee495b02b0cfcde7098701c
Rubí <rubi_salcedocas@outlook.es>
2023-12-20 02:47:24 -0700

    New

---
+++ src/NamingJava.java
@@ -0,0 +1,24 @@
+public class NamingJava {
+    public static void main (String[] args){
+        int celphone = 33387450;
+        int celPhone = 38337447;
+        System.out.println(celphone);
+        System.out.println(celPhone);
+
+        String $coutntryName = "Spain";
+        String backgroundColor = "Green";
+
+        String currency$ = "MXN";
+        String background_color = "Blue";
+
+        int POSITION = -5;
+        int MAX_WIDTH = 9999;
+        int MIN_WIDTH = 1;
+
+
+        //Lower Camel Case
+        String fullName = "Rubi Salcedo Castro";
+        int sizeIncentimeters = 26;
+
+    }
+}
